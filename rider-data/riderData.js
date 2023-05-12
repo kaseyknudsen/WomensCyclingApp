a4590fed65c535b730dde8f43ee33c4d4c52b739
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-11 23:35:34 -0700

    added rider data

---
+++ rider-data/riderData.js
@@ -0,0 +1,63 @@
+export const riderData = [
+    {
+      "name": "Anna van der Breggen",
+      "country": "Netherlands",
+      "team": "SD Worx",
+      "age": 31,
+      "height": "1.73m",
+      "weight": "58kg",
+      "specialty": "climbing",
+      "career_wins": 46,
+      "world_ranking": 1,
+      "social_media": {
+        "twitter": "@AnnavdBreggen",
+        "instagram": "@annavdbreggen"
+      }
+    },
+    {
+      "name": "Marianne Vos",
+      "country": "Netherlands",
+      "team": "Jumbo-Visma",
+      "age": 34,
+      "height": "1.68m",
+      "weight": "58kg",
+      "specialty": "sprinting",
+      "career_wins": 228,
+      "world_ranking": 2,
+      "social_media": {
+        "twitter": "@marianne_vos",
+        "instagram": "@mariannevosofficial"
+      }
+    },
+    {
+      "name": "Annemiek van Vleuten",
+      "country": "Netherlands",
+      "team": "Movistar",
+      "age": 38,
+      "height": "1.69m",
+      "weight": "58kg",
+      "specialty": "time-trialing",
+      "career_wins": 60,
+      "world_ranking": 3,
+      "social_media": {
+        "twitter": "@AvVleuten",
+        "instagram": "@annemiekvanvleuten"
+      }
+    },
+    {
+      "name": "Elisa Longo Borghini",
+      "country": "Italy",
+      "team": "Trek-Segafredo",
+      "age": 30,
+      "height": "1.67m",
+      "weight": "50kg",
+      "specialty": "climbing",
+      "career_wins": 22,
+      "world_ranking": 4,
+      "social_media": {
+        "twitter": "@EllaLongo",
+        "instagram": "@elisalongob"
+      }
+    }, 
+      ]  
+
