025cee3ec274210e362d64ab888bc8c70cafb51a
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-11 23:53:25 -0700

    added correct top 4 rider data

--- rider-data/riderData.js
+++ rider-data/riderData.js
@@ -1,63 +1,58 @@
 export const riderData = [
-    {
-      "name": "Anna van der Breggen",
-      "country": "Netherlands",
-      "team": "SD Worx",
-      "age": 31,
-      "height": "1.73m",
-      "weight": "58kg",
-      "specialty": "climbing",
-      "career_wins": 46,
-      "world_ranking": 1,
-      "social_media": {
-        "twitter": "@AnnavdBreggen",
-        "instagram": "@annavdbreggen"
-      }
+  {
+    name: "Demi Vollering",
+    country: "Netherlands",
+    team: "SD Worx",
+    age: 26,
+    height: "1.72m",
+    specialty: "climbing",
+    world_ranking: 1,
+    social_media: {
+      twitter: "https://twitter.com/demivollering",
+      instagram: "https://www.instagram.com/demivollering/",
+      strava: "https://www.strava.com/athletes/16080090",
     },
-    {
-      "name": "Marianne Vos",
-      "country": "Netherlands",
-      "team": "Jumbo-Visma",
-      "age": 34,
-      "height": "1.68m",
-      "weight": "58kg",
-      "specialty": "sprinting",
-      "career_wins": 228,
-      "world_ranking": 2,
-      "social_media": {
-        "twitter": "@marianne_vos",
-        "instagram": "@mariannevosofficial"
-      }
+  },
+  {
+    name: "Lorenna Wiebes",
+    country: "Netherlands",
+    team: "SD Worx",
+    age: 24,
+    height: "1.71m",
+    specialty: "climbing",
+    world_ranking: 2,
+    social_media: {
+      twitter: "https://twitter.com/lorenawiebes",
+      instagram: "https://www.instagram.com/lorenawiebes/",
+      strava: "https://www.strava.com/athletes/7703167",
     },
-    {
-      "name": "Annemiek van Vleuten",
-      "country": "Netherlands",
-      "team": "Movistar",
-      "age": 38,
-      "height": "1.69m",
-      "weight": "58kg",
-      "specialty": "time-trialing",
-      "career_wins": 60,
-      "world_ranking": 3,
-      "social_media": {
-        "twitter": "@AvVleuten",
-        "instagram": "@annemiekvanvleuten"
-      }
+  },
+  {
+    name: "Annemiek van Vleuten",
+    country: "Netherlands",
+    team: "Movistar",
+    age: 40,
+    height: "1.68m",
+    specialty: "Time Trial",
+    world_ranking: 3,
+    social_media: {
+      twitter: "https://twitter.com/AvVleuten",
+      instagram: "https://www.instagram.com/annemiekvanvleuten/",
+      strava: "",
     },
-    {
-      "name": "Elisa Longo Borghini",
-      "country": "Italy",
-      "team": "Trek-Segafredo",
-      "age": 30,
-      "height": "1.67m",
-      "weight": "50kg",
-      "specialty": "climbing",
-      "career_wins": 22,
-      "world_ranking": 4,
-      "social_media": {
-        "twitter": "@EllaLongo",
-        "instagram": "@elisalongob"
-      }
-    }, 
-      ]  
-
+  },
+  {
+    name: "Elisa Longo Borghini",
+    country: "Italy",
+    team: "Trek Segafreo",
+    age: 31,
+    height: "1.70m",
+    specialty: "Time Trial",
+    world_ranking: 4,
+    social_media: {
+      twitter: "https://twitter.com/ElisaLongoB",
+      instagram: "https://www.instagram.com/elisalongob/",
+      strava: "https://www.strava.com/pros/1189566",
+    },
+  },
+];
